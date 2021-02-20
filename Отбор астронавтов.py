0921224630c94ea4be9b6aa9996ce0465fcbafc7
Иванчик <peregh320@gmail.com>
2021-02-20 21:23:57 +0300

    Отбор астронавтов

--- Отбор астронавтов.py
+++ Отбор астронавтов.py
@@ -5,12 +5,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "<a href='http://127.0.0.1:8080/promotion_image'>Реклама с картинкой</a>"
+    return "<a href='http://127.0.0.1:8080/astronaut_selection'>Отбор астронавтов</a>"
 
 
-@app.route('/promotion_image')
+@app.route('/astronaut_selection')
 def prom_img():
-    return render_template('prom_img.html')
+    return render_template('astronaut.html')
 
 
 if __name__ == '__main__':
